b382bac9a50e6384034442c62cac828d76df129c
Everton Silva <everton.ss7@icloud.com>
2022-07-23 09:56:50 -0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,23 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        //pegar json http
+
+        String url = "https://alura-imdb-api.herokuapp.com/movies";
+
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+
+        String body = response.body();
+
+        System.out.println(body);     
+
+    }
+}
