af7ca080c1396e302c6ac0d2b8c67c70f7f6465e
Everton Silva <everton.ss7@icloud.com>
2022-07-23 12:11:56 -0300

    imersão java aula 02
    Segunda aula imersão java

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -8,10 +10,9 @@ import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        
         //pegar json http
-
-        String url = "https://alura-imdb-api.herokuapp.com/movies";
-
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java/api/TopMovies.json";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
@@ -20,19 +21,23 @@ public class App {
         String body = response.body();          
 
         // extrair só os dados que interessam (Titulo, poster, Classificação)
-
         var parser = new JsonParser();
-
         List<Map<String, String >> ListaDeFilmes = parser.parse(body);
 
         
         //exibir e manipular os dados
-
+        var geradora = new GeradoraDeFigurinhas();
         for (Map<String,String> filme : ListaDeFilmes) {
 
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            geradora.criar(inputStream, nomeArquivo);
+            System.out.println(titulo);
             System.out.println();
             
         }
---
+++ src/GeradoraDeFigurinhas.java
@@ -0,0 +1,49 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.Font;
+
+
+
+
+
+
+public class GeradoraDeFigurinhas {
+    
+    public void criar(InputStream inputStream, String nomeArquivo) throws Exception{
+
+        //leitura da imagem
+        //InputStream inputStrem = new FileInputStream("entrada/filme.jpg");
+        //InputStream inputStrem = new URL("https://imersao-java-apis.s3.amazonaws.com/TopMovies_1.jpg").openStream();
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
+
+        //cria nova imagem em memoria com tranparencia e com tamanho novo
+        int largura = imagemOriginal.getWidth();
+        int altura = imagemOriginal.getHeight();
+        int novoAltura = altura + 200;
+        BufferedImage novaImage = new BufferedImage(largura, novoAltura, BufferedImage.TRANSLUCENT);
+
+
+        //copiar a imagem original para nova imagem
+        Graphics2D graphics = (Graphics2D) novaImage.getGraphics();
+        graphics.drawImage(imagemOriginal, 0, 0, null);
+
+        //configurar a fonte da figurinha
+        var font = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+        graphics.setColor(Color.YELLOW);
+        graphics.setFont(font);
+
+        //escrever uma frase na imagem
+        graphics.drawString("TOPZERA", 100, novoAltura - 100);
+
+        //escrever nova imagem em um arquivo
+        ImageIO.write(novaImage, "png", new File("saida/" + nomeArquivo));
+
+    }
+   
+    
+}
